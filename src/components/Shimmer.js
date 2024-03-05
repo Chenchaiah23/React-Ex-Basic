9a725febdd180c17803cf365e33153aaad5ce870
CHENCHAIAH V <chen.java23@gmail.com>
2024-03-05 14:11:08 +0530

    shimmer changes

--- src/components/Shimmer.js
+++ src/components/Shimmer.js
@@ -1,5 +1,5 @@
 const Shimmer = () => {
-    return <div className="flex mt-10 flex-wrap p-4 m-4 ">
+    return <div className="flex flex-wrap">
         <div className="border bg-gray-400 shadow rounded-3xl  w-1/6   h-80 animate-pulse"/>
         <div className="border bg-gray-400 shadow rounded-3xl  w-1/6   h-80 animate-pulse"/>
         <div className="border bg-gray-400 shadow rounded-3xl  w-1/6   h-80 animate-pulse"/>
